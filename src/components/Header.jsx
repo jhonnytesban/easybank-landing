6a26bf6a190e9ee9a381774c93560984a38abf07
Jonathan <jonathanestebangalan@gmail.com>
2021-02-04 12:48:57 +0100

    diseño movil - fin de menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,35 +3,42 @@ import '../styles/Header.css';
 import logo from '../assets/images/logo.svg';
 import hamburger from '../assets/images/icon-hamburger.svg';
 import close from '../assets/images/icon-close.svg';
+import image from '../assets/images/image-mockups.png';
+import introMobile from '../assets/images/bg-intro-mobile.svg';
 
 class Header extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      style: "active"
+      menu: "active",
+      nav: "active-nav"
     }
   }
   render(){
+    
     const handleStyle = () => {
-      if(this.state.style === "active"){
+      if(this.state.menu === "active"){
         this.setState({
-          style: "deactive"
+          menu: "deactive",
+          nav: "deactive-nav"
         })
-      }else if(this.state.style === "deactive"){
+      }else if(this.state.menu === "deactive"){
         this.setState({
-          style: "active"
+          menu: "active",
+          nav: "active-nav"
         })
       }
     }
+
     return(
       <header className="header">
         <div className="container-header">
           <img className="logo" src={logo} alt="Logo de la empresa EasyBank"/>
           <img className="menu-icon" src={hamburger} alt="Menú de navegación" onClick={handleStyle}/>
-          <img className="close-icon" src={close} alt="Cerrar menú de navegación" onClick={handleStyle}/>
+          <img className="close-icon" src={close} alt="Cerrar menú de navegación"/>
         </div>
-        <nav className="nav">
-          <ul className="menu">
+        <nav className={this.state.nav}>
+          <ul className={this.state.menu}>
             <li className="menu__item"><a href="#" className="link">Home</a></li>
             <li className="menu__item"><a href="#" className="link">About</a></li>
             <li className="menu__item"><a href="#" className="link">Contact</a></li>
@@ -39,6 +46,8 @@ class Header extends React.Component {
             <li className="menu__item"><a href="#" className="link">Careers</a></li>
           </ul>
         </nav>
+        <img className="intro-mobile" src={introMobile} alt=""/>
+        <img className="header__img" src={image} alt=""/>
       </header>
     )
   }
